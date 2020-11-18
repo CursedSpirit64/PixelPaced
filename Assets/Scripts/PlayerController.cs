477681eedf0ed72d77bf19cae2d89fffa9b1a629
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-11-18 10:19:50 -0500

    minor script changes
    controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+
     private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
 
     }
 
