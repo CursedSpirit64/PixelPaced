3a79752b3aaf5f752f910d73ab4af64a3c4c3625
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-11-19 10:33:44 -0500

    New movement script
    An optimal version

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-
+    private Rigidbody rb;
+    public Vector2 movement;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
 
     }
     void Update()
     {
-        moveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
     void moveCharacter(Vector2 direction)
     {
-        transform.Translate(direction * speed * Time.deltaTime);   
+        rb.AddForce(direct * speed);  
     }
 }
