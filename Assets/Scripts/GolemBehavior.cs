2a916adff2c7a19fb294b88d5eac7f6b3c9964e5
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-11-24 10:23:05 -0500

    Script for enemy behavior
    Enemy can now move left to right

--- Assets/Scripts/GolemBehavior.cs
+++ Assets/Scripts/GolemBehavior.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GolemBehavior : MonoBehaviour
 {
+    public float speed;
+
+    public bool MoveRight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,30 @@ public class GolemBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(-2, 2);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(-2, 2);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.GameObject.CompareTag("turn"))
+        {
+            if(MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
     }
 }
