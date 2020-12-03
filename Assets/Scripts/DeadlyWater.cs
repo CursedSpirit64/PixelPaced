14bb5cd66c10bc4b222972054635fb4b52ed3b97
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-12-03 10:21:30 -0500

    Deadly water script

--- Assets/Scripts/DeadlyWater.cs
+++ Assets/Scripts/DeadlyWater.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DeadlyWater : MonoBehaviour
 {
+
+    public float health;
+    public GameObject player; 
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +15,20 @@ public class DeadlyWater : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
     {
         
     }
