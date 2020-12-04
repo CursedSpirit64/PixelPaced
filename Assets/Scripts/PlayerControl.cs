e2a3fe070c87bf0fb9aba99c53a0f7117afa4bce
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-12-04 10:27:40 -0500

    Mario-similar jump

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+
+
+    public float jumpForce;
+    public float jumpTime;
+    public float jumpTimeCounter;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public bool stoppedJumping;
+
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    private Rigidbody2D rb;
+
+void Start()
+{
+    jumpTimeCounter = jumpTime;
+}
+
+void Update()
+{
+    grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+
+    
+    if (grounded)
+    {
+       
+        jumpTimeCounter = jumpTime;
+    }
+}
+
+void FixedUpdate()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+        
+        if (grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
+        }
+    }
+
+    if ((Input.GetMouseButton(0)) && !stoppedJumping)
+    {
+       
+        if (jumpTimeCounter > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    if (Input.GetMouseButtonUp(0))
+    {
+        jumpTimeCounter = 0;
+        stoppedJumping = true;
+    }
+}
+}
+
+}
