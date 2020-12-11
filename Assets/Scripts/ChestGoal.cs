76514d3922d3b46efc52a245c13e38b0bf913300
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-12-11 10:21:43 -0500

    Win Level

---
+++ Assets/Scripts/ChestGoal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestGoal : MonoBehaviour
+{
+    public Text winText;
+    private bool FinishPlane = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        FinishPlane = false;
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            FinishPlane = true;
+            winText.text = "You Win!";
+
+        }
+    }
+}
