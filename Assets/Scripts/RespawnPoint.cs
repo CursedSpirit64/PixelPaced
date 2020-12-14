b3da7e66f0c5d5402343abf87ef7439e5c2b4579
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-12-14 10:52:50 -0500

    stuff

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
+    public GameObject Go;
+    //public GameObject Goa;
+    //public GameObject Gob;
+    //public GameObject Goc;
+    //public GameObject God;
+    //public GameObject Goe;
     [SerializeField] private Transform Player;
     [SerializeField] private Transform respawnPoint;
 
     void OnTriggerEnter(Collider other)
     {
-        //Player.transform.position = respawnPoint.transform.position;
-        Instantiate(hoard, new Vector2(other.transform.position.x, other.transform.position.y), Quaternion.identity);
+        Player.transform.position = respawnPoint.transform.position;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Go);
+        //Instantiate(Goa);
+        //Instantiate(Gob);
+        //Instantiate(Goc);
+        //Instantiate(God);
+        //Instantiate(Goe);
     }
 
     // Update is called once per frame
