701cab1982d09fed207a28f21b68c382dd652178
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-12-17 13:23:48 -0500

    Offset situation

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,12 +2,28 @@
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    //public Transform target;
+    //public float smoothSpeed = 0.125f;
+    public Vector3 cameraOffSet;
+    private GameObject playerObj;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        transform.position = target.position + offset;
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
+
+        //transform.LookAt(target);
+    }
+
+    void Start()
+    {
+        playerObj = GameObject.Find("Player");
+        //cameraOffSet = new Vector3(0, 1, -5);
+    }
+
+    void Update()
+    {
+        transform.position = playerObj.transform.position + cameraOffSet;
     }
 }
